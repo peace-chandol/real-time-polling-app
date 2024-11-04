ca0b4f84e99e95301fcd42da9d2a2d3b31a86296
Peace Chandol <peace.hyperion@gmail.com>
2024-11-04 23:14:46 +0700

    display page

--- fe/display.js
+++ fe/display.js
@@ -5,7 +5,39 @@ socket.on('displayComment', (comment) => {
 })
 
 function addComment(comment) {
-    const container = document.getElementsByClassName('container')
-    block = `<h3>${comment}</h3>`
+    const container = document.getElementsByClassName('inner-comment-container')
+    block = `<div class='display-each-comment'><h3>${comment}</h3></div>`
     container[0].innerHTML += block
 }
+
+
+function setTopic() {
+    const inputTopic = document.getElementById('input-topic')
+    const topicName = document.getElementById('topic-name')
+    topicName.innerHTML = inputTopic.value
+    clearInputTopic()
+}
+
+function clearInputTopic() {
+    const inputTopic = document.getElementById('input-topic')
+    inputTopic.value = ''
+}
+
+function clearTopic() {
+    const topicName = document.getElementById('topic-name')
+    topicName.innerHTML = ''
+}
+
+function clearComment() {
+    const innerCommentContainer = document.getElementsByClassName('inner-comment-container')[0]
+    innerCommentContainer.innerHTML = ''
+}
+
+const saveBtn = document.getElementById('save-btn')
+saveBtn.addEventListener('click', setTopic)
+
+const clearBtn = document.getElementById('clear-btn')
+clearBtn.addEventListener('click', clearTopic)
+
+const clearCommentBtn = document.getElementById('clear-comment-btn')
+clearCommentBtn.addEventListener('click', clearComment)
